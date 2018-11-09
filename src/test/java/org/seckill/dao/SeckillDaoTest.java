bcef925ef7d8c93881ba4a25db191e565816e53e
linran <linran@live.cn>
2018-11-09 22:11:41 +0800

    :art:

---
+++ src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,41 @@
+package org.seckill.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entity.SecKill;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import javax.swing.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * 配置spring和junit整合,junit启动时加载springIOC容器
+ * spring-test, junit
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SeckillDaoTest {
+    //注入Dao实现类依赖
+    @Resource
+    private SeckillDao seckillDao;
+    @Test
+    public void queryById() {
+        long id = 1000;
+        SecKill seckill = seckillDao.queryById(id);
+//        System.out.println(seckill.getName());
+        System.out.println(seckill);
+    }
+
+    @Test
+    public void queryAll() {
+    }
+
+    @Test
+    public void reduceNumber() {
+    }
+
+}
