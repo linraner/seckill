8ab98d72c36841c4320cfa501317e211e1dab333
linran <linran@live.cn>
2018-11-11 10:37:17 +0800

    :art:

--- src/test/java/org/seckill/dao/SeckillDaoTest.java
+++ src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(value = {"classpath:spring/spring-dao.xml"})
@@ -25,9 +27,16 @@ public class SeckillDaoTest {
 
     @Test
     public void queryAll() {
+        List<Seckill> seckillList = seckillDao.queryAll(0, 100);
+        for (Seckill seckill : seckillList) {
+            System.out.println(seckill);
+        }
     }
 
     @Test
     public void reduceNumber() {
+        Date killTime = new Date();
+        int updateCount = seckillDao.reduceNumber(1000L, killTime);
+        System.out.println("updateCount = " + updateCount);
     }
 }
