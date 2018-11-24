f8addea75519b90fbc468dec2f267e3b1494b269
linran <linran@live.cn>
2018-11-24 13:15:18 +0800

    g

---
+++ src/test/java/org/seckill/service/SeckillServiceTest.java
@@ -0,0 +1,41 @@
+package org.seckill.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entity.Seckill;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml",
+                        "classpath:spring/spring-service.xml"})
+public class SeckillServiceTest {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillService seckillService;
+
+    @Test
+    public void getSeckillList() throws Exception {
+        List<Seckill> seckills = seckillService.getSeckillList();
+        System.out.println(seckills);
+    }
+
+    @Test
+    public void getById() {
+    }
+
+    @Test
+    public void exportSeckillUrl() {
+    }
+
+    @Test
+    public void executeSeckill() {
+    }
+}
